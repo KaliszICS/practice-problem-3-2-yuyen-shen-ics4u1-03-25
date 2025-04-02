f8457d17d7250b35865cd27b8f7f2babc4085b11
Yu-Yen, Shen <yu-yen.shen@student.tdsb.on.ca>
2025-04-02 18:45:59 +0000

    yuyen

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,27 +1,16 @@
 public class PracticeProblem {
 
 	public static void main(String args[]) {
-
+		System.out.println(towerOfHanoi(5));
 	}
 
-	public static void q1() {
-		//Write question 1 code here
+	public static int towerOfHanoi(int num) {
+		if (num == 3){
+			return 7;
+		}
+		if (num < 3){
+			return -1;
+		}
+		return 2*(towerOfHanoi(num-1)) + 1;
 	}
-
-	public static void q2() {
-		//Write question 2 code here
-	}
-
-	public static void q3() {
-		//Write question 3 code here
-	}
-
-	public static void q4() {
-		//Write question 4 code here
-	}
-
-	public static void q5() {
-		//Write question 5 code here
-	}
-
 }
